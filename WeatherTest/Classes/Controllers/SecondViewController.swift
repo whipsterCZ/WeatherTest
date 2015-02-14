229307d5056fc1847fd69b3ae290a0324db89daa
WhipsterCZ <whipsterCZ@gmail.com>
2015-02-14 13:11:16 +0100

    cocoapods

---
+++ WeatherTest/Classes/Controllers/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WeatherTest
+//
+//  Created by Daniel Kouba on 14/02/15.
+//  Copyright (c) 2015 Daniel Kouba. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
